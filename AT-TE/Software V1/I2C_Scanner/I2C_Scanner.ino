6df3a015620ae78919c596d1e89aa2f825336e95
Daniel <51463678+Lord7864@users.noreply.github.com>
2025-04-12 03:22:40 -0600

    Added manta foot

---
+++ AT-TE/Software V1/I2C_Scanner/I2C_Scanner.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>              
+
+
+void setup()
+{
+  Wire.begin();
+ 
+  Serial.begin(9600);
+  while (!Serial);             // Leonardo: wait for serial monitor
+  Serial.println("\nI2C Scanner");
+}
+ 
+ 
+void loop()
+{
+  byte error, address;
+  int nDevices;
+ 
+  Serial.println("Scanning...");
+ 
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+  }}
---
+++ AT-TE/Software V1/PCA9685_Control/PCA9685_Control.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <PCA9685.h>
+
+#include "Constants.h"
+
+PCA9685 pca9685;
+
+PCA9685::DurationMicroseconds servo_pulse_duration;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
